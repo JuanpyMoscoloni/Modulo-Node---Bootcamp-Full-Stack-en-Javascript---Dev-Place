fadcf091ada9ddcb4e8d7e779409324614e10e41
JuanpyMoscoloni <moscolonijuanpablo@gmail.com>
2022-11-30 12:24:32 -0300

    30/11

---
+++ Test Moscoloni Juan Pablo/models/recurso.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Joi = require("joi");
+
+const UsersSchema = new Schema({
+  text: {
+    type: String,
+  },
+  idRecurso: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("recurso", UsersSchema);
